469ae6517ea5e7e8d8aba98b03d4a21907280dd3
Smaidziunas <sdamarcius@gmail.com>
2022-11-15 15:34:41 +0200

    task 3 completed

--- Task 3/script.js
+++ Task 3/script.js
@@ -11,4 +11,62 @@ Pastaba: Sukurta kortelė, kurioje yra pateikiama vartotojo informacija, turi
 būti stilizuota su CSS ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'https://api.github.com/users';
+const ENDPOINT = "https://api.github.com/users";
+
+// TAIKAUSI:
+
+const url = "https://api.github.com/users";
+const outputDivEl = document.getElementById("output");
+const showUsersBtnEl = document.getElementById("btn");
+
+// FETCH:
+async function getData(url) {
+  try {
+    const response = await fetch(url);
+    const dataInJsFormat = await response.json();
+    return dataInJsFormat;
+  } catch (error) {
+    console.warn("Error trying to retrieve .then()", error);
+  }
+}
+
+// EVENT LISTENER:
+
+showUsersBtnEl.addEventListener("click", async (event) => {
+  event.preventDefault();
+  // clear "show users" message:
+  outputDivEl.innerHTML = "";
+
+  // Getting data from FETCH
+  fetchedData = await getData(url);
+
+  // Generating Cards:
+  generateCards(fetchedData);
+});
+
+function generateCards(arr) {
+  //creating grid to store all cards:
+  const cardDivEl = document.createElement("div");
+  cardDivEl.className = "grid";
+  outputDivEl.append(cardDivEl);
+
+  // creating and appending cards:
+  arr
+    .map((cardObj) => {
+      const articleEl = document.createElement("article");
+      articleEl.className = "article";
+
+      const liEl1 = document.createElement("li");
+      liEl1.className = "card";
+      liEl1.textContent = `user login: ${cardObj.login}`;
+      const liEl2 = document.createElement("li");
+      liEl2.className = "card";
+      liEl2.textContent = `user's url: ${cardObj.avatar_url}`;
+
+      articleEl.append(liEl1, liEl2);
+      return articleEl;
+    })
+    .forEach((uObj) => {
+      cardDivEl.append(uObj);
+    });
+}
